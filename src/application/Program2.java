0b895becb66b357e33271f1e37f2871162eaf4cb
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2021-07-31 14:08:12 -0300

    Retrieving id | add block finally in Program

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,15 +1,15 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.ParseException;
+import java.sql.Statement;
 import java.text.SimpleDateFormat;
 
 import db.DB;
 
-public class Program {
+public class Program2 {
 
 	public static void main(String[] args) {
 		
@@ -18,14 +18,15 @@ public class Program {
 		Connection conn = null;
 		PreparedStatement st = null;
 		try {
-			
 			conn = DB.getConnection();
 			
-			st = conn.prepareStatement(
+			//Exemplo1			
+/*			st = conn.prepareStatement(
 					"INSERT INTO seller "
 					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
 					+ "VALUES "
-					+ "(?, ?, ?, ?, ?)"					
+					+ "(?, ?, ?, ?, ?)"
+					, Statement.RETURN_GENERATED_KEYS
 					);
 			
 			st.setString(1, "Carl Purple");
@@ -33,19 +34,38 @@ public class Program {
 			st.setDate(3, new Date(sdf.parse("22/04/1985").getTime()));
 			st.setDouble(4, 3000.0);
 			st.setInt(5, 4);
+*/			
+			
+			//Exemplo2
+			st = conn.prepareStatement(
+					"INSERT INTO department (Name) VALUES ('D1'), ('D2')"
+					, Statement.RETURN_GENERATED_KEYS
+					);
 			
 			int rowsAffected = st.executeUpdate();
 			
-			System.out.println("Done! Rows affected: " + rowsAffected);
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while (rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! Id: " + id);
+				}
+			} else {
+				System.out.println("No rows affected!");
+			}
 					
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} catch (ParseException e) {
+			
+/*		} catch (ParseException e) {
 			e.printStackTrace();
-		} finally {
+
+*/
+
+		} finally {		
 			DB.closeStatement(st);
-			DB.closeConnection();
+			DB.getConnection();
 		}
 		
 
